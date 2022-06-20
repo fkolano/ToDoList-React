a3b07199d4c0ab1dc92300594afae80cf296d465
fkolano <fkolano@gmail.com>
2022-06-20 20:08:54 +0000

    final commit

--- src/js/component/todolist.jsx
+++ src/js/component/todolist.jsx
@@ -1,13 +1,52 @@
-import React, { useState} from 'react';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
 
 export const ToDoList = () => {
     const [todoList, setToDoList] = useState([])
     return (
         <div className="container justify-content-center">
             <h1>todos</h1>
-            <input placeholder="What needs to be done"></input>
+           
+            <ul>
+                <li><input placeholder="What needs to be done"></input></li>
+                <li><input placeholder="What needs to be done"></input></li>
+                <li><input placeholder="What needs to be done"></input></li>
+                <li><input placeholder="What needs to be done"></input></li>
+                <li><input placeholder="What needs to be done"></input></li>
+            </ul>
         </div>
     )
+    useEffect(
+        () =>
+         
+          fetch("https://assets.breatheco.de/apis/fake/todos/user/alesanchezr")
+            .then(r => r.json())
+            .then(data => setToDoList(data)),
+        [] 
+      );
+      return (
+        <div>
+          <input
+            onKeyUp={e =>
+              
+              e.keyCode === 13 &&
+              setToDoList(todoList.concat({ label: e.target.value, done: false }))
+            }
+          />
+          <ul>
+            {todoList === null
+              ? "Loading..."
+              : todoList.map(t => (
+                  <li>
+                    {t.label} ({t.done ? "done" : "not done"})
+                  </li>
+                ))}
+          </ul>
+        </div>
+      );
+    };
+    
+    ReactDOM.render(<ToDoList />, document.getElementById("app"));
+    
 
 
-}
